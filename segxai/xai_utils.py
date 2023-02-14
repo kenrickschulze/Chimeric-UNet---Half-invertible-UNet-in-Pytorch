80b3b5bf72c9e2247e1ae09f2768886794cbb58a
Kenrick Schulze <kenrick.schulze@hotmail.de>
2023-02-14 10:58:33 +0100

    Update xai_utils.py
    Removed unused functions

--- segxai/xai_utils.py
+++ segxai/xai_utils.py
@@ -1,69 +1,11 @@
-from socket import if_indextoname
 import numpy as np
 import torch
 from typing import List
 import functools
-# https://github.com/linhaoqi027/SEG-GRAD-CAM/blob/master/gradcam_unet.py
 
-
-class FeatureExtractor:
-    """Class for extracting activations and
-    registering gradients from targetted intermediate layers"""
-
-    def __init__(self, model, target_layer):
-        self.model = model
-        self.target_layer = target_layer
-
-    def get_activation(self, name):
-        def hook(model, inp, output):
-            self.target_activation = inp
-
-        return hook
-
-    def __call__(self, x):
-
-        # register activation
-        # for i, layer in enumerate(list(self.model._modules.keys())):
-        #     if layer == self.target_layer:
-        #         getattr(self.model, layer).register_forward_hook(
-        #             self.get_activation(layer))
-
-        getattr(self.model, self.target_layer).register_forward_hook(
-            self.get_activation(self.target_layer)
-        )
-
-        # perform forward pass
-        out = self.model(x)
-
-        return self.target_activation, out
-
-
-class ModelOutputs:
-    """Class for making a forward pass, and getting:
-    1. The network output.
-    2. Activations from intermeddiate targetted layers.
-    3. Gradients from intermeddiate targetted layers.
-    returns target activations and model output (logits)
-    """
-
-    def __init__(self, model, target_layer):
-        self.model = model
-        self.feature_extractor = FeatureExtractor(self.model, target_layer)
-
-    def get_gradients(self):
-        return self.feature_extractor.gradients
-
-    def __call__(self, x):
-
-        # performs forward pass when called
-        target_activations, x = self.feature_extractor(x)
-
-        return target_activations, x
 
 def normalize(x: np.array) -> np.array:
     return (x - x.min()) / (x.max() - x.min())
-
-
 
 
 def logit2pred(x: torch.Tensor, dim: int = 1) -> torch.Tensor:
